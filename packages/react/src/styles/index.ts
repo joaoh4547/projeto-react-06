99afbe0c26b9d75b8458891657d8d2bd87cd07f1
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-04 22:37:43 -0300

    Capitulo 2 Parte 2

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@joaoh4547-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
