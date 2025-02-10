93e67db605b273c9c20fb2b78dd8b2efb358cd68
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-10 12:56:24 -0300

    Capitulo 3 Parte 4

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarProps } from '@joaoh4547-ui/react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/joaoh4547.png',
+    alt: 'JoaoH4547',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallBack: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
