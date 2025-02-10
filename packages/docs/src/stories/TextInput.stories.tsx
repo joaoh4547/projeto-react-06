c808fdc1a696d39bcb1b289f03d86e841e49a1c2
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-10 18:52:15 -0300

    Capitulo 3 Parte 6

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,39 @@
+import { Box, Text, TextInput, TextInputProps } from '@joaoh4547-ui/react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">User Name</Text>
+          <Story />
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'call.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
