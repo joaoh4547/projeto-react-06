37d72c6cd7b9f4e981bf8a253dc5a1cabdbb6b6d
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-02-11 17:59:25 -0300

    ci: remove token

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   format: ['esm', 'cjs'], // Formatos de saída (ESM e CommonJS)
   dts: true, // Gera arquivos de definição de tipos (.d.ts)
   splitting: false, // Desativa a divisão de código (opcional)
-  sourcemap: true, // Gera sourcemaps (opcional)
+  sourcemap: false, // Gera sourcemaps (opcional)
   clean: true, // Limpa a pasta de saída antes de cada build
   external: ['react', 'react-dom', '@stitches/react'], // Dependências externas
 })
